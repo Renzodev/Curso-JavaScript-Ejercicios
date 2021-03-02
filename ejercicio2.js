ab6f7797e7462a4ecf034f0e44b480e1934e1481
Renzodev <47403887+Renzodev@users.noreply.github.com>
2021-03-02 01:55:17 -0300

    Delete ejercicio2.js

--- ejercicio2.js
+++
@@ -1,7 +0,0 @@
-const caracteres = () =>{
-    let cadena = prompt("Escriba un texto: ");
-    let limite = prompt("Digite el limite de caracteres: ");
-
-    console.log(cadena.substring(0,limite));
-}
-caracteres();
